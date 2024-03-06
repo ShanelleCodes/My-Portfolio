e8cb3c53951d4f17430cb9b08c3aa3ede46ceadc
Shanelle Codes <shanelle.codes@gmail.com>
2024-03-06 17:49:27 -0500

    added gallery page

--- js/index.js
+++ js/index.js
@@ -1,5 +1,40 @@
 const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link')
+const navLinks = document.querySelectorAll('.nav__link');
+
+const lightbox = document.createElement('div')
+lightbox.id = 'lightbox'
+document.body.appendChild(lightbox)
+
+const images = document.querySelectorAll('img.gallery-img')
+images.forEach(image => {
+    image.addEventListener('click', e => {
+        lightbox.classList.add('active')
+        const img = document.createElement('img')
+        img.src = image.src
+        img.alt = image.alt
+
+        // const caption = document.createElement('figcaption');
+        // caption.textContent = image.getAttribute('data-caption');
+
+        while (lightbox.firstChild){
+            lightbox.removeChild(lightbox.firstChild)
+        }
+
+        lightbox.appendChild(img)
+
+        if(image.dataset.caption){
+            const caption = document.createElement('figcaption')
+            caption.textContent = image.dataset.caption
+            lightbox.appendChild(caption);
+        }
+        
+})
+})
+
+lightbox.addEventListener('click', e => {
+    if (e.target !== e.currentTarget) return
+    lightbox.classList.remove('active')
+})
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
@@ -10,3 +45,6 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     })
 });
+
+
+
